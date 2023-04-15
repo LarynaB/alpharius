8bb612bf9393f3b6b1e297d0214273ea01f863b3
Laryna <lunchbox1990@hotmail.com>
2023-04-15 16:11:15 -0700

    banner placeholder

--- splash.py
+++ splash.py
@@ -1,6 +1,37 @@
+#class name from module
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return '''
+        <html>
+            <head>
+                <style>
+                    #banner {
+                        width: 100%;
+                        height: 150px;
+                        background-color: #333;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    #banner img {
+                        max-height: 100%;
+                        max-width: 100%;
+                        object-fit: contain;
+                    }
+                </style>
+            </head>
+            <body>
+                <div id="banner">
+                    <img src="https://placekitten.com/1200/150" alt="cat image">
+                </div>
+                <h1>Hello, World!</h1>
+            </body>
+        </html>
+    '''
+#run the app
+if __name__ == '__main__':
+    app.run()
 
