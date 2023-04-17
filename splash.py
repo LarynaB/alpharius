fc45f31836f2b64dfce4c2994525e729a5d0e5e9
Laryna <lunchbox1990@hotmail.com>
2023-04-16 23:31:44 -0700

    navigation bar

--- splash.py
+++ splash.py
@@ -1,4 +1,3 @@
-#class name from module
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,6 +7,7 @@ def home():
         <html>
             <head>
                 <style>
+                    /* Styles for the banner */
                     #banner {
                         width: 100%;
                         height: 150px;
@@ -21,16 +21,45 @@ def home():
                         max-width: 100%;
                         object-fit: contain;
                     }
+                    /* Styles for the navigation bar */
+                    #nav {
+                        width: 100%;
+                        height: 50px;
+                        background-color: #eee;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        list-style-type: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    #nav li {
+                        margin-right: 10px;
+                    }
+                    #nav li a {
+                        color: #333;
+                        text-decoration: none;
+                    }
+                    #nav li a:hover {
+                        color: #555;
+                        text-decoration: underline;
+                    }
                 </style>
             </head>
             <body>
                 <div id="banner">
                     <img src="https://placekitten.com/1200/150" alt="cat image">
                 </div>
+                <ul id="nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
                 <h1>Hello, World!</h1>
             </body>
         </html>
     '''
+
 #run the app
 if __name__ == '__main__':
     app.run()
